88d7c1c2f4ddf554552a93c9e0887ea773856123
mithon <mithonphilip@gmail.com>
2024-03-13 14:34:40 +0600

    rename folder

---
+++ 08-DataType/number.js
@@ -0,0 +1,40 @@
+// ************** Number *************
+var n = 12323;
+var f = 3.14;
+var nn = Number("232");
+// console.log(nn);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+var x = 0.2 + 0.1;
+console.log(x);
+
+var xx = (0.2 + 10 + 0.1 * 10) / 10;
+console.log(xx);
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2);
+
+let num3 = "100";
+let num4 = "10";
+console.log(num3 - num4);
+
+let num5 = "10";
+let num6 = "10";
+console.log(num5 * num6);
+
+let divide = 100 / "Apple";
+console.log(divide);
+
+let divideString = 100 / "10";
+console.log(divideString);
+
+console.log(typeof NaN);
+
+let xy = 2/0;
+let yt = -2/0;
+console.log(xy, yt);
+
+console.log(typeof Infinity);
+
+console.log(0xFF);
