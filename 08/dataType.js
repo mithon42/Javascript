fb4fa93a41f6698af7c48b13b100c1f9493fff31
Md. Mithon Ali <82331340+mithon42@users.noreply.github.com>
2024-03-13 01:19:11 -0700

    Add files via upload

---
+++ 08/dataType.js
@@ -0,0 +1,17 @@
+// primitive data type
+// object data type
+
+/** Primitive
+ * Number
+ * String
+ * ``
+ * Boolean = true, false
+ * undefined = null
+*/
+
+
+/** Object
+ *  Array
+ * Object
+ * Function
+*/
